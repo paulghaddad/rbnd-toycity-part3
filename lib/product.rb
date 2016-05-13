d06878e613a98f435ad2c05b3c30b973c9a9b916
Paul Haddad <paulh16@gmail.com>
2016-05-13 07:13:16 -0500

    Implement exception if a product title already exists

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,30 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    if duplicate_title?
+      raise DuplicateProductError, "#{title} already exists."
+    end
+
+    @@products << self
+  end
+
+  def duplicate_title?
+    @@products.each do |product|
+      product.title == title
+    end
   end
 end
