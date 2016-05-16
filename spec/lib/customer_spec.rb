a10d429cb28b6b14632559ad3d3e479e6f8c8e9b
Paul Haddad <paulh16@gmail.com>
2016-05-16 07:12:59 -0500

    Implement Customer initialize method

--- spec/lib/customer_spec.rb
+++ spec/lib/customer_spec.rb
@@ -1,5 +1,11 @@
 require "spec_helper"
 
 describe Customer do
+  describe "#new" do
+    it "creates a new customer with a name" do
+      customer = Customer.new(name: "Paul Haddad")
 
+      expect(customer).to have_attributes(name: "Paul Haddad")
+    end
+  end
 end
