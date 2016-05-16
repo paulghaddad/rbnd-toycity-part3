1b48af589334a59566e31ad970a8c3873b33eb04
Paul Haddad <paulh16@gmail.com>
2016-05-16 07:50:51 -0500

    Implement Customer duplicate name error and Customer.find_by_name

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,43 @@
 class Customer
   attr_reader :name
 
+  @customers = []
+
+  class << self
+    attr_accessor :customers
+  end
+
   def initialize(name:)
     @name = name
+    add_to_customers_registry
+  end
+
+  def self.all
+    Customer.customers
+  end
+
+  def self.find_by_name(name)
+    Customer.customers.find do |customer|
+      customer.name == name
+    end
+  end
+
+  private
+
+  def add_to_customers_registry
+    check_for_duplicate_customer
+    Customer.customers << self
+  end
+
+  def duplicate_customer?
+    Customer.customers.any? do |customer|
+      customer.name == name
+    end
+  end
+
+  def check_for_duplicate_customer
+    if duplicate_customer?
+      raise DuplicateCustomerError, "#{self.name} already exists"
+    end
   end
 end
--- spec/lib/customer_spec.rb
+++ spec/lib/customer_spec.rb
@@ -1,11 +1,47 @@
 require "spec_helper"
 
 describe Customer do
-  describe "#new" do
-    it "creates a new customer with a name" do
-      customer = Customer.new(name: "Paul Haddad")
 
-      expect(customer).to have_attributes(name: "Paul Haddad")
+  before do
+    Customer.customers = []
+  end
+
+  describe "#new" do
+    context "not a duplicate name" do
+      it "creates a new customer with a name" do
+        customer = Customer.new(name: "Paul")
+
+        expect(customer).to have_attributes(name: "Paul")
+      end
+
+      context "duplicate name" do
+        it "raises an error" do
+          Customer.new(name: "Paul")
+
+          expect { Customer.new(name: "Paul") }.to raise_error(DuplicateCustomerError)
+        end
+      end
+    end
+
+    describe ".all" do
+      it "returns all the customers" do
+        customer_1 = Customer.new(name: "Customer 1")
+        customer_2 = Customer.new(name: "Customer 2")
+
+        customers = Customer.all
+
+        expect(customers).to contain_exactly(customer_1, customer_2)
+      end
+    end
+
+    describe ".find_by_name" do
+      it "returns the customer of the provided name" do
+        Customer.new(name: "Paul Haddad")
+
+        customer = Customer.find_by_name("Paul Haddad")
+
+        expect(customer.name).to eq("Paul Haddad")
+      end
     end
   end
 end
